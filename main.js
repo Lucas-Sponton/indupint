7479bf15b3b5ef66255b8ef1d4a5f18458277ef5
Lucas Sponton <lucas.sponton6@gmail.com>
2024-08-25 20:00:26 -0300

    Creación de los productos del json, faltan agregar

--- main.js
+++ main.js
@@ -8,10 +8,10 @@ function cargarProductos() {
                 const card = document.createElement("div");
                 card.classList.add("col-3");
                 card.innerHTML = `
-                        <div class="card main__card">
+                        <div class="card main__productCard">
                             <div class="card-body">
-                                <h1 class="card-title">${producto.id}</h1>
-                                <h2 class="card-subtitle mb-2 tituloProducto"><b>$${producto.precio}</b></h2>
+                                <h1 class="card-title fs-4">${producto.nombre}</h1>
+                                <h2 class="card-subtitle mb-2 fs-5 tituloProducto"><b>${producto.rendimiento}</b></h2>
                                 <p class="card-text">${producto.descripcion}</p>
                             </div>
                         </div>
