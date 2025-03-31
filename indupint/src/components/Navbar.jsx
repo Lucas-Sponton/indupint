8cf2425eb853c0d9fc063441c6df94c78f07a383
Lucas Sponton <lucas.sponton6@gmail.com>
2025-03-31 16:49:56 -0300

    prueba 3 error de imagenes

--- indupint/src/components/Navbar.jsx
+++ indupint/src/components/Navbar.jsx
@@ -28,7 +28,7 @@ const NavBar = () => {
             <div className="row">
                 <div className="col-3 navBar__responsive">
                     <NavLink to={"/"} onClick={handleLogoClick}>
-                        <img src="../assets/titulo-indupint" alt="hola soy el logo" className="header__navLogo" />
+                        <img src="public\titulo-indupint.png" alt="hola soy el logo" className="header__navLogo" />
                     </NavLink>
                 </div>
                 <div className="col d-flex justify-content-end my-2">
